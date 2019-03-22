2c70c5b361221e60f4ed4a76ed302e52fc9ff2c0
formation <formation@FORM274>
2019-03-22 14:37:59 +0100

    ajout css pics js + masterPage

---
+++ Projet1Parent/Projet1Web/src/main/webapp/js/brouillonPourTests.js
@@ -0,0 +1,18 @@
+// ANIMATION DASHBOARD CARDS
+
+//Animate the element's value from x to y:
+$({ someValue: 0 }).animate({ someValue: mbReportingAdmin.nbreEtablissements }, {
+    duration: 3000,
+    easing: 'swing', // can be anything
+    step: function () { // called on every step
+        // Update the element's text with rounded-up value:
+        $('.count').text(commaSeparateNumber(Math.round(this.someValue)));
+    }
+});
+
+function commaSeparateNumber(val) {
+    while (/(\d+)(\d{3})/.test(val.toString())) {
+        val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    }
+    return val;
+}
